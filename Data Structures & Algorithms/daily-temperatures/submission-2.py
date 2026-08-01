4d1cd4eab04e366f260da156b5c332eca8f53948
Sunay Konnur <konnursunay@gmail.com>
2026-08-01 15:38:51 +0000

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # Create monotonic stack - decreasing
+        # When new element is added, pop all of the elemnts that are smaller than it
+
+        stack = []
+        ret = [0] * len(temperatures)
+        for idx, temp in enumerate(temperatures):
+            while stack and stack[-1][0] < temp:
+                _, old_idx = stack.pop()
+                ret[old_idx] = idx - old_idx
+            stack.append((temp, idx))
+
+        return ret
+            
